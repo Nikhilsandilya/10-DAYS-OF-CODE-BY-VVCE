9cd2524f1cbfe2469bc715c9a9237267b38a4a59
Nikhilsandilya <69521392+Nikhilsandilya@users.noreply.github.com>
2020-08-20 19:28:48 +0530

    Add files via upload

---
+++ equal stacks.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+
+int n1,n2,n3,i;
+
+cin>>n1>>n2>>n3;
+int p=0,q=0,r=0;
+int a[n1],b[n2],c[n3];
+long long sum=0,sum1=0,sum2=0;
+for(i=0;i<n1;i++)
+    {
+     cin>>a[i];
+    sum=sum+a[i];
+    }
+
+for(i=0;i<n2;i++)
+   {
+   cin>>b[i];
+    sum1=sum1+b[i];
+    }
+for(i=0;i<n3;i++)
+    {
+     cin>>c[i];
+    sum2=sum2+c[i];
+    }
+
+     while(sum!=sum1 ||sum1!=sum2 ||sum!=sum2)
+    {
+    if(sum>sum1 && sum>sum2|| sum==sum1 && sum>sum2 || sum==sum2 &&sum>sum1)
+            sum=sum-a[p++];
+          if(sum1>sum && sum1>sum2|| sum1==sum && sum1>sum2 || sum1==sum2 &&sum1>sum)
+            sum1=sum1-b[q++];
+          if(sum2>sum &&sum2>sum1 || sum2==sum1 && sum2>sum || sum2==sum &&sum2>sum1)
+            sum2=sum2-c[r++]; }
+cout<<sum;
+   return 0;
+}
